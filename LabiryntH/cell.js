0a16a147047cb261f785876fd7626da7c31e42c9
Florian3k <diaxplayer47@gmail.com>
2017-01-28 15:10:40 +0100

    Add files via upload

---
+++ LabiryntH/cell.js
@@ -0,0 +1,75 @@
+function index(i, j) {
+  if (i < 0 || j < 0 || i > cols-1 || j > rows-1) {
+    return -1;
+  }
+  return i + j * cols;
+}
+
+function Cell(i, j) {
+	this.i = i;
+	this.j = j;
+  this.walls = [true, true, true, true];
+  this.visited = false;
+
+  this.highlight = function(cR,cG,cB) {
+    var x = this.i * w;
+		var y = this.j * w;
+    noStroke();
+    fill(cR,cG,cB);
+    rect(x+1,y+1,w-1,w-1);
+  }
+
+  this.checkNeighbours = function() {
+    var neighbours = [];
+    var top    = grid[index(i    , j - 1)];
+    var right  = grid[index(i + 1, j    )];
+    var bottom = grid[index(i    , j + 1)];
+    var left   = grid[index(i - 1, j    )];
+
+    if (top && !top.visited) {
+      neighbours.push(top);
+    }
+    if (right && !right.visited) {
+      neighbours.push(right);
+    }
+    if (bottom && !bottom.visited) {
+      neighbours.push(bottom);
+    }
+    if (left && !left.visited) {
+      neighbours.push(left);
+    }
+
+    if (neighbours.length > 0) {
+      var r = floor(random(0,neighbours.length));
+      return neighbours[r];
+    } else {
+      return undefined;
+    }
+  }
+
+  this.show = function() {
+		var x = this.i * w;
+		var y = this.j * w;
+		stroke(51);
+
+    if (this.walls[0]) {
+		  line(x    , y    , x + w, y    );
+    }
+    if (this.walls[1]) {
+		  line(x + w, y    , x + w, y + w);
+    }
+    if (this.walls[2]) {
+		  line(x + w, y + w, x    , y + w);
+    }
+    if (this.walls[3]) {
+		  line(x    , y + w, x    , y    );
+    }
+
+    //if(this.visited) {
+    //  noStroke();
+    //  fill(219, 46, 69);
+		//  rect(x+1,y+1,w,w);
+    //}
+	}
+
+}
---
+++ LabiryntH/sketch.js
@@ -0,0 +1,116 @@
+var cols, rows;
+var w = 10;
+var grid = [];
+var stack = [];
+var generating = true;
+var current;
+var winCell;
+
+function setup() {
+	createCanvas(601, 601);
+	cols = floor(width/w);
+	rows = floor(height/w);
+	for (var j = 0; j < rows; j++) {
+		for (var i = 0; i < rows; i++) {
+			var cell = new Cell(i,j);
+			grid.push(cell);
+		}
+	}
+	current = grid[0];
+	winCell = grid[grid.length-1];
+	current.visited = true;
+	while (current) {
+		var next = current.checkNeighbours();
+		if (next) {
+			// Step 2
+			stack.push(current);
+			// Step 3
+			removeWalls(current,next);
+			//Step 4
+			current = next;
+			next.visited = true;
+		} else {
+			current = stack.pop();
+		}
+	}
+	current = grid[0];
+}
+
+function draw() {
+	background(200);
+	for (var i = 0; i < grid.length; i++) {
+		grid[i].show();
+	}
+	//current = grid[0];
+	//alert("Steruj strzałkami aby się poruszać. Dotrzyj do niebieskiego kwadratu. POWODZENIA :D");
+	current.highlight(0,255,0);
+
+		if (current === winCell) {
+			WinXD();
+		}
+		winCell.highlight(0,0,255);
+		if (keyIsPressed === true) {
+			switch (keyCode) {
+				case UP_ARROW:
+					console.log("up");
+					if (current.walls[0] === false) {
+						current = grid[index(current.i,current.j-1)];
+					}
+					break;
+				case RIGHT_ARROW:
+					console.log("right");
+					if (current.walls[1] === false) {
+						current = grid[index(current.i+1,current.j)];
+					}
+					break;
+				case DOWN_ARROW:
+					console.log("down");
+					if (current.walls[2] === false) {
+						current = grid[index(current.i,current.j+1)];
+					}
+					break;
+				case LEFT_ARROW:
+					console.log("left");
+					if (current.walls[3] === false) {
+						current = grid[index(current.i-1,current.j)];
+					}
+					break;
+			}
+		}
+
+	noFill();
+	stroke(51);
+	rect(0,0,600,600);
+}
+
+function removeWalls(a,b) {
+	var x = a.i - b.i;
+	var y = a.j - b.j;
+
+	if (x === 1) {
+		a.walls[3] = false;
+		b.walls[1] = false;
+	} else if (x === -1) {
+		a.walls[1] = false;
+		b.walls[3] = false;
+	} else if (y === 1) {
+		a.walls[0] = false;
+		b.walls[2] = false;
+	} else if (y === -1) {
+		a.walls[2] = false;
+		b.walls[0] = false;
+	}
+}
+
+function WinXD() {
+	noLoop();
+	alert("Wygrałeś xD")
+}
+
+
+
+
+
+
+
+//
