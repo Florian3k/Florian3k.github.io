44ada6ffd2282747ec01741cd2a45b158e102bdf
Florian3k <diaxplayer47@gmail.com>
2017-01-29 16:58:35 +0100

    Add files via upload

---
+++ Fireworks/firework.js
@@ -0,0 +1,39 @@
+function Firework() {
+  this.hue = random(255);
+  this.firework =
+  new Particle(random(width),height,this.hue,true);
+  this.exploded = false;
+  this.particles = [];
+
+  this.update = function() {
+    if (!this.exploded) {
+      this.firework.applyForce(gravity);
+		  this.firework.update();
+      if (this.firework.vel.y >= 0) {
+        this.expolde();
+      }
+    }
+    for (var i = 0; i <this.particles.length; i++) {
+      this.particles[i].applyForce(gravity);
+      this.particles[i].update();
+    }
+  }
+
+  this.show = function() {
+    if (!this.exploded) {
+      this.firework.show();
+    }
+    for (var i = 0; i <this.particles.length; i++) {
+      this.particles[i].show();
+    }
+  }
+
+  this.expolde = function() {
+    this.exploded = true;
+    for (var i = 0; i < 100; i++) {
+      this.particles.push(new Particle
+        (this.firework.pos.x,
+          this.firework.pos.y, this.hue));
+    }
+  }
+}
---
+++ Fireworks/particle.js
@@ -0,0 +1,44 @@
+function Particle(x, y, hue, firework) {
+  this.hue = hue;
+  this.pos = createVector(x, y);
+  this.lifespan = 255;
+  this.firework = firework;
+  //this.vel = createVector(0, -12);
+  if (this.firework) {
+    this.vel = createVector(0, random(-22,-11));
+  } else {
+    this.vel = p5.Vector.random2D();
+    this.vel.mult(random(2,7));
+  }
+
+  this.acc = createVector(0,0);
+
+  this.applyForce = function(force) {
+    this.acc.add(force);
+  }
+  this.update = function() {
+    if (!this.firework) {
+      this.vel.mult(0.9);
+      this.lifespan -= 4;
+      if (this.lifespan < 125) {
+        this.lifespan -= 4;
+      }
+    }
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  this.show = function() {
+    if (this.firework) {
+      strokeWeight(3);
+    } else {
+      strokeWeight(2);
+    }
+    stroke(hue + random(-40, 40),
+      255 + random(-10, 10),
+      this.lifespan + random(-10, 10) );
+    point(this.pos.x, this.pos.y);
+  }
+
+}
