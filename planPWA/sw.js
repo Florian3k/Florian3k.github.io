5e4fba7678cfbcba2b8b9a75f854226555fd113c
Florian3k <24961583+Florian3k@users.noreply.github.com>
2018-09-04 08:39:12 +0200

    Add files via upload

---
+++ planPWA/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'app-cache';
+
+const urlsToCache = [
+	// 'index.html',
+	// './dist/app.bundle.js',
+	// './dist/lib.bundle.js',
+	// './src/js/projects.js'
+];
+
+self.addEventListener('install', (event) => {
+	// event.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+	// 	return cache.addAll(urlsToCache);
+	// }));
+});
+
+self.addEventListener('fetch', (event) => {
+	// event.respondWith(caches.match(event.request).then((response) => {
+	// 	return response ? response : fetch(event.request);
+	// }));
+	event.respondWith(fetch(event.request));
+});
